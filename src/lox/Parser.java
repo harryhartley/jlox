85bcd5b5fa2b48d93389677c496f361bb100e899
harryhartley <hhartley94@gmail.com>
2023-01-27 15:50:45 +0000

    parser match

--- src/lox/Parser.java
+++ src/lox/Parser.java
@@ -25,4 +25,15 @@ class Parser {
 
     return expr;
   }
+
+  private boolean match(TokenType... types) {
+    for (TokenType type : types) {
+      if (check(type)) {
+        advance();
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
